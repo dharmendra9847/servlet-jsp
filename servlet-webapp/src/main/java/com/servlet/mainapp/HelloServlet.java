a2f2d08f0bfa9b601977255d6cab20ba270e988b
Dharmendra Kumar <dharmendra9847@gmail.com>
2026-05-03 12:17:00 +0530

    feat: add html content

--- servlet-webapp/src/main/java/com/servlet/mainapp/HelloServlet.java
+++ servlet-webapp/src/main/java/com/servlet/mainapp/HelloServlet.java
@@ -23,13 +23,22 @@ public class HelloServlet extends HttpServlet {
 
         PrintWriter writer = resp.getWriter();
 
-        // Without html
-        writer.println("Hello, " + name);
-        writer.println("I know you're from " + city);
+        // Without HTML
+        //writer.println("Hello, " + name);
+        //writer.println("I know you're from " + city);
 
-        // With html
-        writer.println("<h1>Hello, " + name + "!</h1>");
-        writer.println("<h3>I know you're from " + city + "!</h3>");
+        // With HTML without body
+        //writer.println("<h1>Hello, " + name + "!</h1>");
+        //writer.println("<h3>I know you're from " + city + "!</h3>");
+
+        // With HTML with body
+        writer.println("<html> <head> <title>My-Servlet-WebApp</title> </head>");
+        writer.println("<body bgcolor='cyan'> <h1><marquee>Hello, Welcome to My Servlet Web Application with Dynamic Response!</marquee></h1>");
+        writer.println("<table border='1'>");
+        writer.println("<tr> <th>Name</th> <th>City</th> </tr>");
+        writer.println("<tr><td>" + name + "</td><td>" + city + "</td></tr>");
+        writer.println("</table>");
+        writer.println("</body> </html>");
 
         writer.close();
     }
