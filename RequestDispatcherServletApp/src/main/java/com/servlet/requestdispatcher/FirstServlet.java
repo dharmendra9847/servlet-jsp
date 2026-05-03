7e199f1983c6ec941dd304307a76612f51973cc9
Dharmendra Kumar <dharmendra9847@gmail.com>
2026-05-03 20:42:07 +0530

    Implement Session

--- RequestDispatcherServletApp/src/main/java/com/servlet/requestdispatcher/FirstServlet.java
+++ RequestDispatcherServletApp/src/main/java/com/servlet/requestdispatcher/FirstServlet.java
@@ -1,10 +1,11 @@
 package com.servlet.requestdispatcher;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,7 +14,7 @@ import java.io.PrintWriter;
 public class FirstServlet extends HttpServlet {
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
         System.out.println("Control in First-Servlet!");
 
@@ -21,19 +22,23 @@ public class FirstServlet extends HttpServlet {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
 
-        if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+        // Implement session here
+        HttpSession session = req.getSession();
+        session.setAttribute("fullname", name);
+        session.setAttribute("email", email);
+        session.setAttribute("password", password);
 
-        }
+        session.setMaxInactiveInterval(10); // session expired here within 10 seconds
 
-        //req.getRequestDispatcher("/second-servlet").forward(req, res);
+        req.getRequestDispatcher("/second-servlet").forward(req, res);
 
-        req.getRequestDispatcher("/second-servlet").include(req,res);
+        //req.getRequestDispatcher("/second-servlet").include(req,res);
 
         PrintWriter writer = res.getWriter();
         writer.println("<h1>Response From First-Servlet</h1>");
 
         writer.close();
 
-        System.out.println("Control Again Goes in First-Servlet!");
+        //System.out.println("Control Again Goes in First-Servlet!");
     }
 }
--- RequestDispatcherServletApp/src/main/java/com/servlet/requestdispatcher/SecondServlet.java
+++ RequestDispatcherServletApp/src/main/java/com/servlet/requestdispatcher/SecondServlet.java
@@ -1,10 +1,11 @@
 package com.servlet.requestdispatcher;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,12 +14,21 @@ import java.io.PrintWriter;
 public class SecondServlet extends HttpServlet {
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
         System.out.println("Control in Second-Servlet!");
 
+        // Implement session here
+        HttpSession session = req.getSession(false);
+        String name = (String) session.getAttribute("fullname");
+        String email = (String) session.getAttribute("email");
+        String password = (String) session.getAttribute("password");
+
         PrintWriter writer = res.getWriter();
         writer.println("<h1>Response From Second-Servlet</h1>");
+        writer.println("<h2>" + name + "</h2>");
+        writer.println("<h2>" + email + "</h2>");
+        writer.println("<h2>" + password + "</h2>");
 
         writer.close();
 
